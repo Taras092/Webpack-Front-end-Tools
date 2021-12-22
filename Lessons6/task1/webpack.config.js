b45e07aa7576c1e1e909e8301a8cc5b2f43f7e19
taras <taraslychynets@icloud.com>
2021-12-22 13:53:23 +0200

    Webpack - CSS loader

---
+++ Lessons6/task1/webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "./bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // {
+      //   test: /\.sass$/,
+      //   use: ["style-loader", "sass-loader"]
+      // },
+      // {
+      //   test: /\.scss$/,
+      //   use: ["style-loader", "sass-loader"]
+      // },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
+            }
+          }
+        ],
+      },
+    ],
+  },
+};
